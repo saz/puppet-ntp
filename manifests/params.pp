5e3fad4291ed8d040aa582f8a0a18df97c2a0ed4
Michael Arnold <github@razorsedge.org>
2012-03-26 23:18:13 -0700

    Added supoprt for ntpdate.
    Added supoprt for the separate ntpdate package and "service" that will set the
    clock before ntpd starts.

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,6 +11,11 @@ class ntp::params {
       $config_file = '/etc/ntp.conf'
       $service_name = 'ntpd'
       $driftfile = '/var/lib/ntp/drift'
+
+      $ntpdate_package = 'ntpdate'
+      $ntpdate_config_file = '/etc/ntp/step-tickers'
+      $ntpdate_defaults_file = '/etc/sysconfig/ntpdate'
+      $ntpdate_service_name = 'ntpddate'
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
