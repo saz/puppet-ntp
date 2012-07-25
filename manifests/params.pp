9ca4c0182ddcc87dd50fc13a89e07179688b5f63
Marcello Barnaba <vjt@openssl.it>
2012-07-25 15:30:40 +0200

    Parametrize config file owner and mode

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,22 +1,29 @@
 class ntp::params {
+  # Common
+  $package = 'ntp'
+  $config_file = '/etc/ntp.conf'
+
   case $::operatingsystem {
     /(Ubuntu|Debian)/: {
-      $package = 'ntp'
-      $config_file = '/etc/ntp.conf'
       $service_name = 'ntp'
       $driftfile = '/var/lib/ntp/ntp.drift'
+      $config_file_owner = 'root'
+      $config_file_group = 'root'
+      $config_file_mode = 0644
     }
     'RedHat', 'Fedora', 'CentOS', 'Scientific', 'SLC', 'Ascendos', 'PSBM', 'OracleLinux', 'OVS', 'OEL': {
-      $package = 'ntp'
-      $config_file = '/etc/ntp.conf'
       $service_name = 'ntpd'
       $driftfile = '/var/lib/ntp/drift'
+      $config_file_owner = 'root'
+      $config_file_group = 'root'
+      $config_file_mode = 0644
     }
     OpenSuSE: {
-      $package = 'ntp'
-      $config_file = '/etc/ntp.conf'
       $service_name = 'ntp'
       $driftfile = '/var/lib/ntp/drift/ntp.drift'
+      $config_file_owner = 'root'
+      $config_file_group = 'ntp'
+      $config_file_mode = 0640
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
