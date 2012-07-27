679b6ca40bb67baa76a8a0332c350fb45f490b56
System Administrator (on puppet.ifad.org) <root@ifad.org>
2012-07-27 14:25:40 +0200

    Use single quotes for config_file_mode

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,21 +9,21 @@ class ntp::params {
       $driftfile = '/var/lib/ntp/ntp.drift'
       $config_file_owner = 'root'
       $config_file_group = 'root'
-      $config_file_mode = 0644
+      $config_file_mode  = '0644'
     }
     'RedHat', 'Fedora', 'CentOS', 'Scientific', 'SLC', 'Ascendos', 'PSBM', 'OracleLinux', 'OVS', 'OEL': {
       $service_name = 'ntpd'
       $driftfile = '/var/lib/ntp/drift'
       $config_file_owner = 'root'
       $config_file_group = 'root'
-      $config_file_mode = 0644
+      $config_file_mode  = '0644'
     }
     OpenSuSE: {
       $service_name = 'ntp'
       $driftfile = '/var/lib/ntp/drift/ntp.drift'
       $config_file_owner = 'root'
       $config_file_group = 'ntp'
-      $config_file_mode = 0640
+      $config_file_mode  = '0640'
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
